3f7cc32ddb8fdad92bdacaa22c7c718952ca5aa6
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-11-30 03:48:51 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+<<<<<<< HEAD
   def create
     @post = Post.new
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
     redirect_to post_path(@post)
+=======
+
+  def create
+    post = Post.new
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
+>>>>>>> 5fcb40478915fd9beecd3ef6dc9d1689a4470780
   end
 end
